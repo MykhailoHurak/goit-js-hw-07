bdcb9bb6e9b60c1132142dd0a3b61b3eaf284e5a
Mikhail Gurak <mikhail.gurak@gmail.com>
2022-04-29 22:33:53 +0300

    hw7 task 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,64 +1,63 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї
-
-const galleryRef = document.querySelector('.gallery');
-
-const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+const galleryList = document.querySelector('.gallery');
+const itemsMarkup = galleryItems.map(({ preview, original, description}) => {
   return `
   <div class="gallery__item">
-   <a class="gallery__link" href="${original}">
-    <img class="gallery__image" src="${preview}" data-source="large-image.jpg" alt="${description}" />
-   </a>
-  </div>
-  `
-}).join('');
-
-galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
-
-// 2. Реалізація делегування на div.gallery і отримання url великого зображення.
-
-galleryRef.addEventListener('click', handlerGalleryRefClick);
-function handlerGalleryRefClick() { };
-
-// Завдання 1 - галерея зображень ============================================================================
-
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. 
-// Подивися демо відео роботи галереї.
-
-// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js.
-// Розбий його на декілька підзавдань:
-// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// 2. Реалізація делегування на div.gallery і отримання url великого зображення.
-// 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. 
-// Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
-// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
-// Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
-// Розмітка елемента галереї
-// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. 
-// Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
-
-{/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</div> */}
+</div>
+`
+}).join('');
 
-// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку.
-// Заборони цю поведінку за замовчуванням.
+galleryList.insertAdjacentHTML('afterbegin', itemsMarkup);
 
-// Закриття з клавіатури
-// УВАГА: Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
-// Додай закриття модального вікна після натискання клавіші Escape. 
-// Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
-// Бібліотекаи basicLightbox містить метод для програмного закриття модального вікна.
+galleryList.addEventListener('click', onGalleryListClick);
+
+let instance;
+
+function onGalleryListClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return
+  };
+
+  const url = getOricinalImgUrl(event);
+  instance = createModal(url);
+  
+  instance.show(window.addEventListener('keydown', onEscClick));
+};
+
+function createModal(url) {
+  return basicLightbox.create(`
+    <img src="${url}">
+  `,
+    {
+      onClose: () => {
+        window.removeEventListener('keydown', onEscClick);
+      }
+    }
+  );
+};
+
+function getOricinalImgUrl(e) {
+  return e.target.dataset.source;
+};
+
+function onEscClick(e) {
+  const isEsc = e.code !== 'Escape';
+
+  if (isEsc) {
+    return
+  };
+
+  instance.close();
+}
